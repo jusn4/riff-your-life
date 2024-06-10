e240ebb3502354f101df2c9d90a249e6b6103598
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-10 11:05:37 +0000

    モデルの記述(未migrate)

--- db/migrate/20240610104456_create_posts.rb
+++ db/migrate/20240610104456_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      t.string :title, null: false
+      t.text :body, null: false
+      t.integer :user_id, null: false
 
       t.timestamps
     end
