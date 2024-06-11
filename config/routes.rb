0b03e2cd5a45010b777906dac52fb8b730970b97
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-11 09:35:11 +0000

    make sign_in,up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
     #skip以降の記述によりパスワード変更、管理者登録のルーティングの削除
     # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
-    registrations: "admins/registrations",
-    sessions: "admins/sessions",
-    passwords: "admins/passwords",
-    confirmations: "admins/confirmations"
+    registrations: "admin/registrations",
+    sessions: "admin/sessions",
+    passwords: "admin/passwords",
+    confirmations: "admin/confirmations"
   }
   devise_for :users, controllers: {
     # ↓ローカルに追加されたコントローラーを参照する(コントローラー名: "コントローラーの参照先")
-    registrations: "users/registrations",
-    sessions: "users/sessions",
-    passwords: "users/passwords",
-    confirmations: "users/confirmations"
+    registrations: "public/registrations",
+    sessions: "public/sessions",
+    passwords: "public/passwords",
+    confirmations: "public/confirmations"
   }
 end
