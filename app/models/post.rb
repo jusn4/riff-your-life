ab1679a025e2a3a46104279c37b5f6643dc57a68
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-14 13:39:35 +0000

    fix error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,14 @@
 class Post < ApplicationRecord
+   belongs_to :user
   
-  #mount_uploader :music, AudiofileUploader #音声ファイルの取り込み
   has_one_attached :music
   has_one_attached :image #画像の取り込み
   
-  belongs_to :user
-  
-  def get_image
-    if image.attached?
-      image
-    else
+  def get_image(width,height)
+    unless image.attached?
       file_path = Rails.root.join('app/assets/images/noImage.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+    image.variant(resize_to_limit: [width, height]).processed
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  has_one_attached :image
+         
   has_many :posts, dependent: :destroy
 
-  def get_image
-    if image.attached?
-      image
-    else
+  has_one_attached :image
+  
+
+  def get_image(width,height)
+    unless image.attached?
       file_path = Rails.root.join('app/assets/images/noImage.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
+    image.variant(resize_to_limit: [width,height]).processed
   end
 
 
