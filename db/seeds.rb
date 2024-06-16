02182900a729915d2b10e80395f35749979de1b1
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-16 12:21:05 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-User.create!(
-  email: 'test@test',
-  name: 'タカマサ',
-  password: 'example'
-  )
-
-#Admin.find_or_create_by!(email: "admin@admin.com",password: 'example')
-Admin.find_or_create_by!(email: 'admin@admin') do |admin|
-  admin.password = 'example'
-end
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -16,3 +5,17 @@ end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ puts "seedの実行を開始"
+  User.find_or_create_by!(email: "test@test") do |user|
+    user.name = "タカマサ"
+    user.password = "password"
+    user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/app/assets/images/icon.png"), filename:"sample-user1.jpg")
+  end
+
+#Admin.find_or_create_by!(email: "admin@admin.com",password: 'example')
+Admin.find_or_create_by!(email:ENV['ADMIN_EMAIL'] ) do |admin|
+  admin.password = ENV['ADMIN_PASSWORD']
+end
+
+puts "seedの実行が完了しました"
