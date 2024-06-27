ed0e8ee07a0606711ab42d66be6111cece5b0eed
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-27 05:29:23 +0000

    add page

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,11 +7,11 @@ class Admin::PostsController < ApplicationController
 
   def index
     if params[:word].present?
-      @posts = Post.where('title LIKE ?', "%#{params[:word]}%")
+      @posts = Post.where('title LIKE ?', "%#{params[:word]}%").page(params[:page])
     elsif params[:tag_id].present?
-      @posts = Tag.find(params[:tag_id]).posts
+      @posts = Tag.find(params[:tag_id]).page(params[:page])
     else
-      @posts = Post.all
+      @posts = Post.page(params[:page])
     end
   end
 
