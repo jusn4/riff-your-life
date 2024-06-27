d7a3d1eaa9c8acc6fa3db3e40971f5b55a58e542
jusna <164776044+jusn4@users.noreply.github.com>
2024-06-27 12:41:46 +0000

    fix error

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -14,7 +14,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to mypage_path
+    redirect_to posts_path
   end
   # before_action :configure_sign_in_params, only: [:create]
 
